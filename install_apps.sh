c5e23303db3612f6078992050696d41aed7d0068
Mike Jetter <mikejetter@yahoo.com>
2020-09-20 20:12:17 -0400

    added install_apps.sh and install_dotfiles.sh scripts

---
+++ install_apps.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y build-essential
+sudo apt install -y git
+sudo apt install -y tmux
+sudo apt install -y htop
+sudo apt install -y vlc
+sudo apt install -y ipcalc
+sudo apt install -y ansible
+sudo apt install -y vim
+sudo apt install -y tree
+sudo apt install -y sysbench
+sudo apt install -y hdparm
